b9a20b7aa77f982e2ab249fe0efab440f72198c7
Julia Tan <julia@joinzoe.com>
2020-11-07 23:05:45 +0000

    Calculate number of times Man United won... but written without Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import fs from 'fs';
+import fs from "fs";
 
-const matches = fs.readFileSync('football.csv', {
-  // give us a string with the content of the CSV file
-  encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => {
-  return row.split(',')
-})
+const matches = fs
+  .readFileSync("football.csv", {
+    // give us a string with the content of the CSV file
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
 
-console.log(matches)
+let manUnitedWins = 0;
+
+// Count number of times Man United won
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === "A") {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
