0ca9a0631f78a83b70330d0b53dcf9ca332ac0bb
Julia Tan <julia@joinzoe.com>
2020-11-07 22:55:04 +0000

    Import CSV and parse data into array of array of strings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi')
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  // give us a string with the content of the CSV file
+  encoding: 'utf-8'
+}).split('\n').map((row: string): string[] => {
+  return row.split(',')
+})
+
+console.log(matches)
