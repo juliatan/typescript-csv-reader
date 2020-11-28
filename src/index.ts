c734d61c2af8697cad0bb324811bc7c674e01bdf
Julia Tan <julia@joinzoe.com>
2020-11-28 19:08:34 +0000

    Introduce enum to make code more readable

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,22 @@ const matches = fs
     return row.split(",");
   });
 
+// enum automatically creates a new Type called MatchResult
+// enums are useful to tell other devs in the team that these are closely related values. Use only when we know all
+// the possible outcomes - cannot dynamically change an enum config during runtime.
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+};
+
 let manUnitedWins = 0;
 
 // Count number of times Man United won
 for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
